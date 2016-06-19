ff4c7238a589d472d8d0d096ebfd7a69fad15448
Sergey Smolin <soees84@gmail.com>
2016-06-19 19:21:09 +0300

    Create Procfile, change styles and server settings

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 9999));
+
 app.use("/static", express.static('build'));
 
 app.get("*", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('The app is listening on port', app.get('port'));
 });
